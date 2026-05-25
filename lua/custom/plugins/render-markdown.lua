aa7c6d55c49918d39b7264f7bd07ddafcd17f6c2
Kylie Benfield <kylie@revelryplay.com>
2026-05-24 19:40:09 -0600

    Adding nvim-highlight-colors and render-markdown plugins
    Extracted as standalone files in lua/custom/plugins/ so they don't
    conflict with upstream init.lua changes on rebase.

---
+++ lua/custom/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },
+    ft = { 'markdown' },
+    opts = {
+      latex = { enabled = false },
+    },
+  },
+}
